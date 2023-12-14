8406647975faf97c4db1a466868dce1f819a7ec8
unknown <bruno.jimenez@laplateforme.io>
2023-12-14 19:10:41 +0100

    CSS in working and display with grid in current working

--- FinanceFlow/src/App.jsx
+++ FinanceFlow/src/App.jsx
@@ -20,22 +20,48 @@ const App = () => {
 
   return (
     <ResponsiveComponent>
-      <div className='box'>
-          <img className='logotitleun' src="../logo.png" alt="Photo de plage vue du dessus" />
-          <h1 className='logotitleun'>Gestionnaire de Budget</h1>
-      <p className='bouchetrou logotitleun' id='un'></p>
+      <div>
+          <div className='Section-header'>
+            <img src="../logo.png" alt="Photo de plage vue du dessus" />
+            <h1 >Gestionnaire de Budget</h1>
+            <p id='un'></p>
+          </div>
+        <div className='Section-principale'>
+          {/* Composant pour ajouter une nouvelle opération */}
+          <AddNewOperation addTransaction={addTransaction} />
 
-        {/* Composant pour ajouter une nouvelle opération */}
-        <AddNewOperation addTransaction={addTransaction} />
+          {/* Composant pour filtrer les transactions */}
+          <TransactionDataFilter transactions={transactions} categories={categories} />
 
-        {/* Composant pour filtrer les transactions */}
-        <TransactionDataFilter transactions={transactions} categories={categories} />
+          {/* Utiliser le composant TransactionLogs pour afficher la liste des transactions et le solde restant */}
+          <TransactionLogs transactions={transactions} clearHistory={clearHistory}/>
 
-        {/* Utiliser le composant TransactionLogs pour afficher la liste des transactions et le solde restant */}
-        <TransactionLogs transactions={transactions} clearHistory={clearHistory}/>
-
-        {/* Utiliser le composant TransactionLogs pour afficher la liste des transactions et le solde restant */}
-        <HowMuch transactions={transactions}/>
+          {/* Utiliser le composant TransactionLogs pour afficher la liste des transactions et le solde restant */}
+          <HowMuch transactions={transactions}/>
+        </div>
+        
+        <div className='Section-secondaire'>
+        
+        
+        
+        </div>
+          
+        <div className='Section-footer'>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus nihil quam debitis dolorum, 
+          ratione dolore distinctio dolor, nostrum impedit quaerat explicabo sint! Nemo tempore qui ducimus autem dolorem molestias perspiciatis?</p>
+          
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, dicta minima? 
+          Id iste soluta dignissimos! Molestiae sunt laborum maxime est neque magni in corrupti, eaque ipsam doloribus placeat! Maxime, in.</p>
+          
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, alias. Repellendus, 
+          quia quasi amet blanditiis mollitia sed provident architecto deleniti esse officiis,
+          cupiditate obcaecati dicta culpa laudantium ipsam aperiam fuga.</p>
+          
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quos incidunt ad saepe,
+          eum deserunt officia commodi, itaque velit reprehenderit inventore error placeat atque necessitatibus.
+          Esse, harum. Dicta, eaque itaque.</p> 
+        </div>  
+          
       </div>
     </ResponsiveComponent>
   );
